e30d0635619fbe2fe00abe910305f3c2ad745aa6
F2re <f2re@users.noreply.github.com>
2026-08-21 08:12:48 +0300

    refactor: убрать временную защиту компоновки

--- public/organization-layout-guard.js
+++
@@ -1,7 +0,0 @@
-function cleanOrganizationLayout() {
-  document.querySelectorAll('.topbar-actions [data-organization-open]').forEach((node) => node.remove());
-  document.querySelectorAll('.organization-indent[style]').forEach((node) => node.removeAttribute('style'));
-}
-
-cleanOrganizationLayout();
-new MutationObserver(cleanOrganizationLayout).observe(document.body, { childList: true, subtree: true });
